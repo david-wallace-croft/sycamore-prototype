18b1c8c1cd6567aae1ab3b189ead27d14d0358e1
David Wallace Croft <david@CroftSoft.com>
2022-08-13 11:10:48 -0500

    start conversion of Angular demo

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,36 @@ use sycamore::prelude::*;
 
 #[component]
 fn App<G: Html>(cx: Scope) -> View<G> {
-  let name = create_signal(cx, String::new());
+  let banner_width = create_signal(cx, 123);
+  let time_remaining = create_signal(cx, 456);
   div()
     .c(
       h1()
-        .t("Hello ")
-        .dyn_if(
-          || !name.get().is_empty(),
-          || span().dyn_t(|| name.get().to_string()),
-          || span().t("World"),
-        )
-        .t("!"),
+        .attr("class", "app-header")
+        .t("Application Title [")
+        .dyn_t(|| banner_width.get().to_string())
+        .t("px]"),
     )
-    .c(input().bind_value(name))
+    .c(
+      span()
+        .attr("class", "time-remaining")
+        .dyn_t(|| time_remaining.get().to_string()),
+    )
     .view(cx)
+  //   let name = create_signal(cx, String::new());
+  //   div()
+  //     .c(
+  //       h1()
+  //         .t("Hello, ")
+  //         .dyn_if(
+  //           || !name.get().is_empty(),
+  //           || span().dyn_t(|| name.get().to_string()),
+  //           || span().t("World"),
+  //         )
+  //         .t("!"),
+  //     )
+  //     .c(input().bind_value(name))
+  //     .view(cx)
 }
 
 fn main() {
