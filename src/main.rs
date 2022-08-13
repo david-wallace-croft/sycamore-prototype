ed8297fbe126d730152f5fc2bc87e6f89cdea1eb
David Wallace Croft <david@CroftSoft.com>
2022-08-13 13:13:52 -0500

    form 1 component

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ fn App<G: Html>(cx: Scope) -> View<G> {
     )
     .c(div().class("app-content").c(div().class("app-navbar")))
     .c(ClickWrapComponent(cx))
+    .c(Form1Component(cx))
     .c(ContactComponent(cx))
     .c(PrivacyComponent(cx))
     .view(cx)
@@ -133,6 +134,37 @@ fn ContactComponent<G: Html>(cx: Scope) -> View<G> {
     .c(p().t(LOREM_IPSUM))
     .c(h3().t("Phone"))
     .c(p().t(LOREM_IPSUM))
+    .view(cx)
+}
+
+#[component]
+fn Form1Component<G: Html>(cx: Scope) -> View<G> {
+  div()
+    .c(h2().t("Form 1 Title"))
+    .c(h3().t("All fields are required"))
+    .c(
+      form().c(
+        div()
+          .class("form-group")
+          .c(
+            label()
+              .class("control-label")
+              .attr("for", "idn")
+              .t("Identity Number"),
+          )
+          .c(
+            input()
+              .class("form-control")
+              .id("idn")
+              .attr("maxlength", "11")
+              .attr("name", "idn")
+              .attr("pattern", "^(\\d{9}|(\\d{3}-\\d{2}-\\d{4}))$")
+              .attr("placeholder", "[characters masked]")
+              .attr("required", "required")
+              .attr("type", "password"),
+          ),
+      ),
+    )
     .view(cx)
 }
 
