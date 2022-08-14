2dbe941ce70ed59965841b8b9033d6d148e7f65e
David Wallace Croft <david@CroftSoft.com>
2022-08-14 09:32:26 -0500

    show mask toggle

--- src/main.rs
+++ src/main.rs
@@ -117,20 +117,6 @@ fn AppComponent<G: Html>(cx: Scope) -> View<G> {
     // .c(ContactComponent(cx))
     // .c(PrivacyComponent(cx))
     .view(cx)
-  //   let name = create_signal(cx, String::new());
-  //   div()
-  //     .c(
-  //       h1()
-  //         .t("Hello, ")
-  //         .dyn_if(
-  //           || !name.get().is_empty(),
-  //           || span().dyn_t(|| name.get().to_string()),
-  //           || span().t("World"),
-  //         )
-  //         .t("!"),
-  //     )
-  //     .c(input().bind_value(name))
-  //     .view(cx)
 }
 
 #[component]
@@ -227,31 +213,109 @@ fn ContactComponent<G: Html>(cx: Scope) -> View<G> {
 
 #[component]
 fn Form1Component<G: Html>(cx: Scope) -> View<G> {
+  let show: &Signal<bool> = create_signal(cx, false);
   div()
     .c(h2().t("Form 1 Title"))
     .c(h3().t("All fields are required"))
     .c(
-      form().c(
-        div()
-          .class("form-group")
-          .c(
-            label()
-              .class("control-label")
-              .attr("for", "idn")
-              .t("Identity Number"),
-          )
-          .c(
-            input()
-              .class("form-control")
-              .id("idn")
-              .attr("maxlength", "11")
-              .attr("name", "idn")
-              .attr("pattern", "^(\\d{9}|(\\d{3}-\\d{2}-\\d{4}))$")
-              .attr("placeholder", "[characters masked]")
-              .attr("required", "required")
-              .attr("type", "password"),
-          ),
-      ),
+      form()
+        .c(
+          div()
+            .class("form-group")
+            .c(
+              label()
+                .class("control-label")
+                .attr("for", "idn")
+                .t("Identity Number"),
+            )
+            .c(
+              input()
+                .class("form-control")
+                .id("idn")
+                .attr("maxlength", "11")
+                .attr("name", "idn")
+                .attr("pattern", "^(\\d{9}|(\\d{3}-\\d{2}-\\d{4}))$")
+                .attr("placeholder", "[characters masked]")
+                .attr("required", "required")
+                .attr("type", "password"),
+            ),
+        )
+        .c(
+          button()
+            .class("btn btn-default")
+            .attr("type", "button")
+            .dyn_if(
+              || *show.get(),
+              || span().dyn_t(|| "Mask"),
+              || span().dyn_t(|| "Show"),
+            )
+            .on("click", |_| show.set(!*show.get()))
+            .dyn_if(
+              || *show.get(),
+              || {
+                span().dyn_c(|| {
+                  svg()
+            .attr("alt", "")
+            .attr("fill", "#000")
+            .attr("focusable", "false")
+            .attr("height", "24")
+            .attr("role", "presentation")
+            .attr("viewBox", "0 0 24 24")
+            .attr("width", "24")
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .c(
+              path()
+                .attr("alt", "")
+                .attr("d", "M0 0h24v24H0z")
+                .attr("fill", "none")
+                .attr("role", "presentation"),
+            )
+            .c(
+              path()
+                .attr("alt", "")
+                .attr("d",
+    "M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26
+    2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16
+    2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1
+    12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73
+    3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3
+    .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5
+    0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z")
+                .attr("role", "presentation"),
+            )
+                })
+              },
+              || {
+                span().dyn_c(|| {
+                  svg()
+            .attr("alt", "")
+            .attr("fill", "#000")
+            .attr("focusable", "false")
+            .attr("height", "24")
+            .attr("role", "presentation")
+            .attr("viewBox", "0 0 24 24")
+            .attr("width", "24")
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .c(
+              path()
+                .attr("alt", "")
+                .attr("d", "M0 0h24v24H0z")
+                .attr("fill", "none")
+                .attr("role", "presentation"),
+            )
+            .c(
+              path()
+                .attr("alt", "")
+                .attr("d",
+    "M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11
+    11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24
+    5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z")
+                .attr("role", "presentation"),
+            )
+                })
+              },
+            ),
+        ),
     )
     .view(cx)
 }
